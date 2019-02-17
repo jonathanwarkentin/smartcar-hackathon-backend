1168f5424b9b309c43736935bcc1f3f2b082e38b
Jonathan <warkentin.jon@gmail.com>
2019-02-17 00:19:54 -0600

    add basic api functionality on index page

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return '<h1>Smartcar App Dependencies Installed</h1>'
+    if (request.method == 'POST'):
+        some_json = request.get_json()
+        return jsonify({'you sent': some_json}), 201
+    else:
+        return jsonify({"about":"Hello World!"})
