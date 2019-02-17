ba7fe445fd333c9034e1b51a93f9a0f6c85a1927
Jonathan <warkentin.jon@gmail.com>
2019-02-17 01:08:17 -0600

    try again

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from flask import Flask, redirect, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/', methods=['GET', 'POST'])
+@cross_origin(origin='https://shrouded-ravine-72981.herokuapp.com/',headers=['Content- Type','Authorization'])
 def index():
     if (request.method == 'POST'):
         some_json = request.get_json()
         response = jsonify({'you sent': some_json})
-        response.headers.add('Access-Control-Allow-Origin', '*')
         return response, 201
     else:
         response = jsonify({"about":"Hello World!"})
-        response.headers.add('Access-Control-Allow-Origin', '*')
         return jsonify({"about":"Hello World!"})
